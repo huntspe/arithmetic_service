462dec87d588d2111c592401fd45e09125df9930
hunnspe <hunnspe@gmail.com>
2024-02-15 16:57:39 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+});
+
+app.get('/add/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) + Number(req.params.m));
+});
+
+app.get('/sub/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) - Number(req.params.m));
+});
+
+app.get('/mul/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) * Number(req.params.m));
+});
+
+app.get('/div/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) / Number(req.params.m));
+});
+
 app.get('/eval/:expression', (req, res) => {
     const expression = req.params.expression;
     try {
@@ -7,3 +34,9 @@ app.get('/eval/:expression', (req, res) => {
         res.status(400).json({ error: "Invalid expression" });
     }
 });
+
+app.listen(port, () => {
+    console.log(`Arithmetic service running at http://localhost:${port}`);
+});
+
+module.exports = app;
