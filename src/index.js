4e1d9fb6b26785f51c50c36f327cedf5233d6cd0
hunnspe <hunnspe@gmail.com>
2024-02-15 16:51:48 -0500

    adding calculator

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Arithmetic service - Hello World!');
+app.get('/eval/:expression', (req, res) => {
+    const expression = req.params.expression;
+    try {
+        const result = eval(expression); // Using eval to evaluate the expression
+        res.json(result);
+    } catch (error) {
+        res.status(400).json({ error: "Invalid expression" });
+    }
 });
-
-app.get('/add/:n/:m', (req, res) => { // any http request with url /add/[number]/[number]
-    res.json(Number(req.params.n) + Number(req.params.m));
-});
-
-app.listen(port);
-
-// test number 2
